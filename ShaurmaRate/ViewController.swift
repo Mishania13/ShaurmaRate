9ac1618bf9089a87a18217a4dc899d5af26df098
Mishania13 <mishk777@yandex.ru>
2020-03-25 19:56:23 +0300

    Initial Commit

---
+++ ShaurmaRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShaurmaRate
+//
+//  Created by Mr. Bear on 25.03.2020.
+//  Copyright © 2020 Mr. Bear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
