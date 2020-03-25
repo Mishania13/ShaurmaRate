f608eac3dfa5474b6ba8dee3f8e5abb522d38786
Mishania13 <mishk777@yandex.ru>
2020-03-25 21:21:23 +0300

    Make lockable app with image and shawm title

---
+++ ShaurmaTVC.swift
@@ -0,0 +1,36 @@
+//
+//  ShaurmaTVC.swift
+//  ShaurmaRate
+//
+//  Created by Mr. Bear on 25.03.2020.
+//  Copyright © 2020 Mr. Bear. All rights reserved.
+//
+
+import UIKit
+
+class ShaurmaTVC: UITableViewController {
+    
+    var popShavaPlaces = [ "Евро Кебаб", "Мастер Кебаб" , "На Лиговском",
+                           "Хорошая шаверма", "У Захара", "Гирос", "Шеф шаверма"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Table view data source
+  
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        popShavaPlaces.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = popShavaPlaces[indexPath.row]
+        cell.imageView?.image = UIImage(named: popShavaPlaces[indexPath.row])
+        return cell
+    }
+    
+    
+    
+}
